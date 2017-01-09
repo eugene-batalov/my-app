91045fc71bffef14a8f66ba55c0f5c97422836c2
batalov <batalov@inpolus.ru>
2017-01-09 23:05:31 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+ReactDOM.render(
+  element,//<App />,
+  document.getElementById('root')
+);
